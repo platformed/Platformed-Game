89cc269071c97bf64ed09265fdd350a37faa2c61
Kyler Smith <kylersmith7@gmail.com>
2015-06-20 16:44:50 -0700

    Added basic camera movement controls

---
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+	public float speed = -1f;
+
+	void Start () {
+	
+	}
+
+	void LateUpdate () {
+		transform.LookAt (Camera.main.transform);
+		float h = Input.GetAxis("Horizontal") * speed;
+		float v = Input.GetAxis("Vertical") * speed;
+		transform.Translate(new Vector3(h, 0.0f, v));
+	}
+}
---
+++ Assets/Scripts/Camera/CameraOrbit.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraOrbit : MonoBehaviour {
+	
+	public Transform target;
+	public float distance = 5.0f;
+	public float xSpeed = 40f;
+	public float ySpeed = 350f;
+	
+	public float yMinLimit = -90f;
+	public float yMaxLimit = 90f;
+	
+	public float distanceMin = 2f;
+	public float distanceMax = 25f;
+	
+	private Rigidbody rigidbody;
+	
+	float x = 0.0f;
+	float y = 0.0f;
+
+	void Start () {
+		Vector3 angles = transform.eulerAngles;
+		x = angles.y;
+		y = angles.x;
+		
+		rigidbody = GetComponent<Rigidbody>();
+		
+		// Make the rigid body not change rotation
+		if (rigidbody != null) {
+			rigidbody.freezeRotation = true;
+		}
+	}
+	
+	void LateUpdate () {
+		if (target) 
+		{
+			if((Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)) && (Input.GetMouseButton(0) || Input.GetMouseButton(2))){
+				x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.1f / distance;
+				y -= Input.GetAxis("Mouse Y") * ySpeed * 0.1f / distance;
+			}
+			
+			y = ClampAngle(y, yMinLimit, yMaxLimit);
+			
+			Quaternion rotation = Quaternion.Euler(y, x, 0);
+			
+			distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
+			
+			RaycastHit hit;
+			if (Physics.Linecast (target.position, transform.position, out hit)) 
+			{
+				distance -=  hit.distance;
+			}
+			Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+			Vector3 position = rotation * negDistance + target.position;
+			
+			transform.rotation = rotation;
+			transform.position = position;
+		}
+	}
+	
+	public static float ClampAngle(float angle, float min, float max){
+		if (angle < -360F) {
+			angle += 360F;
+		}
+		if (angle > 360F) {
+			angle -= 360F;
+		}
+		return Mathf.Clamp(angle, min, max);
+	}
+}
