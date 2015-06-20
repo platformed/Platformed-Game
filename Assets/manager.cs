390a26eb2b945da70b1bcd3397a4f88ca76f8e82
Kyler Smith <kylersmith7@gmail.com>
2015-06-20 10:27:15 -0700

    Started main menu

---
+++ Assets/manager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class manager : MonoBehaviour {
+	public void changeScene(string scene){
+		Application.LoadLevel (scene);
+	}
+
+	public void exit(){
+		Application.Quit ();
+	}
+}
