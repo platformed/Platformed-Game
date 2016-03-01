5a01bde817a8ca1e8e4894ef446f819f33a49137
Kyler Smith <kylersmith7@gmail.com>
2016-02-29 22:07:34 -0800

    Block editor

--- Assets/Scripts/Settings/Items/SliderSettingsItem.cs
+++ Assets/Scripts/Settings/Items/SliderSettingsItem.cs
@@ -2,13 +2,13 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class SwitchSettingsItem : SettingsItem {
-	public SwitchSettingsItem(string label) : base(label) {
-		
+public class SliderSettingsItem : SettingsItem {
+	public SliderSettingsItem(string label) : base(label) {
+
 	}
 
 	public override void Draw(Transform parent) {
-		GameObject instace = Object.Instantiate(Resources.Load("UI/Settings/Switch") as GameObject);
+		GameObject instace = Object.Instantiate(Resources.Load("UI/Settings/Slider") as GameObject);
 		instace.transform.SetParent(parent);
 
 		instace.GetComponentInChildren<Text>().text = GetLabel();
--- Assets/Scripts/Settings/Settings.cs
+++ Assets/Scripts/Settings/Settings.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 
 public class Settings {
 	List<SettingsItem> items = new List<SettingsItem>();
+	Transform itemPanel;
 
-	public Settings() {
-
+	public Settings(Transform itemPanel) {
+		this.itemPanel = itemPanel;
 	}
 
 	public void AddItem(SettingsItem item) {
 		items.Add(item);
+		item.Draw(itemPanel);
 	}
 }
