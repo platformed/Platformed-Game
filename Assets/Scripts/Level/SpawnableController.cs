57b4004123f231b0655cfa510cacbc764f37d456
Kyler Smith <kylersmith7@gmail.com>
2016-04-02 18:10:34 -0700

    Change camera speeds

---
+++ Assets/Scripts/Level/SpawnableController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnableController : MonoBehaviour {
+	SpawnableBlock block;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
