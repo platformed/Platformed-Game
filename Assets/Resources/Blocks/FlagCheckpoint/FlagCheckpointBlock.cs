a708ea2d082782e939203c3cb50152f47b93a554
Kyler Smith <kylersmith7@gmail.com>
2016-06-28 12:11:15 -0700

    Checkpoint and finish flag blocks

---
+++ Assets/Resources/Blocks/FlagCheckpoint/FlagCheckpointBlock.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlagCheckpointBlock : SpawnableBlock {
+	public FlagCheckpointBlock() {
+		Name = "FlagCheckpoint";
+		DisplayName = "Checkpoint Flag";
+    }
+}
