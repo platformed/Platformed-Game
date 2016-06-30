379f8ddc5ff381d2e0a37eaf5bfe542982129563
Snowfall24 <omglegobuilder@gmail.com>
2016-06-30 14:26:45 -0700

    Changes.1.0.10

---
+++ Assets/Scripts/Play/PlayerHealth.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+    public float Health;
+    public int Lives;
+
+	// Use this for initialization
+	void Start () {
+        Lives = 3;
+        Health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Health <= 0)
+        {
+            Respawn();
+        }
+        if(transform.position.y <= -10)
+        {
+            Respawn();
+        }
+        
+        
+        
+	}
+    void Respawn ()
+    {
+        transform.position = new Vector3(128, 138, 128);
+        if(Lives > 0)
+        {
+            Lives -= 1;
+        }
+    }
+}
