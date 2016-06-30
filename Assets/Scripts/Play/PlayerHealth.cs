a230dfc7429f4fcdd80188eee8423f45269ab26e
Snowfall24 <omglegobuilder@gmail.com>
2016-06-30 14:56:00 -0700

    Working on lives bar

--- Assets/Scripts/Play/PlayerHealth.cs
+++ Assets/Scripts/Play/PlayerHealth.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnitySystems.UI;
 
 public class PlayerHealth : MonoBehaviour {
+    public static PlayerHealth instance;
     public float Health;
     public int Lives;
+    public int Lives { get; set; }
+
+    public Text livesText;
 
 	// Use this for initialization
 	void Start () {
         Lives = 3;
         Health = 100;
 	}
+    void Awake()
+    {
+        instance = this;
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        livesText.text = lives.tostring
         if (Health <= 0)
         {
             Respawn();
